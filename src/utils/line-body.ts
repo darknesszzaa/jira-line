4ac4eb646bfad697ab82602c472af59b7c139f14
Anucha Prapphrom <anucha.p@rvconnex.com>
2021-08-22 02:51:40 +0700

    fest() add line webhook log time

--- src/utils/line-body.ts
+++ src/utils/line-body.ts
@@ -59,81 +59,85 @@ function logTimeToday(replyToken, logTimeHours) {
   const body = {
     replyToken: replyToken,
     messages: [{
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://lh3.googleusercontent.com/proxy/p9I4GYIwp88S2RlhhRc35zIuPpuDeYNrkl9oPeRUHrgWw7IMtt0UGJHDXY1et6r6BJlB3fz83y9pC7wGBloZdYGEfwuUBCm5Xsa2nFU8kE5i_WHEelQcgNU3Kd2bwStmiDBImb4cXtfccWLtvU6J_DRiPK8Eo7YGWAmRTRK-",
-        "size": "full",
-        "aspectMode": "cover",
-        "action": {
-          "type": "uri",
-          "uri": "http://linecorp.com/"
-        },
-        "aspectRatio": "20:13"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Log Time Today",
-            "weight": "bold",
-            "size": "xl"
+      type: 'flex',
+      altText: 'LogTime',
+      contents: {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: "https://lh3.googleusercontent.com/proxy/p9I4GYIwp88S2RlhhRc35zIuPpuDeYNrkl9oPeRUHrgWw7IMtt0UGJHDXY1et6r6BJlB3fz83y9pC7wGBloZdYGEfwuUBCm5Xsa2nFU8kE5i_WHEelQcgNU3Kd2bwStmiDBImb4cXtfccWLtvU6J_DRiPK8Eo7YGWAmRTRK-",
+          size: "full",
+          aspectMode: "cover",
+          action: {
+            type: "uri",
+            uri: "http://linecorp.com/"
           },
-          {
-            "type": "box",
-            "layout": "vertical",
-            "margin": "lg",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "box",
-                "layout": "baseline",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Log TIme",
-                    "size": "md",
-                    "flex": 4,
-                    "weight": "regular"
-                  },
-                  {
-                    "type": "text",
-                    "text": logTimeHours + " Hr.",
-                    "wrap": true,
-                    "size": "lg",
-                    "flex": 5,
-                    "color": "#FF0000"
-                  }
-                ]
-              },
-              {
-                "type": "box",
-                "layout": "baseline",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Update Time",
-                    "size": "md",
-                    "flex": 4
-                  },
-                  {
-                    "type": "text",
-                    "text": new Date(),
-                    "wrap": true,
-                    "size": "md",
-                    "flex": 5
-                  }
-                ],
-                "paddingTop": "xl"
-              }
-            ],
-            "paddingTop": "xl"
-          }
-        ]
+          aspectRatio: "20:13"
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "Log Time Today",
+              weight: "bold",
+              size: "xl"
+            },
+            {
+              type: "box",
+              layout: "vertical",
+              margin: "lg",
+              spacing: "sm",
+              contents: [
+                {
+                  type: "box",
+                  layout: "baseline",
+                  spacing: "sm",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "Log TIme",
+                      size: "md",
+                      flex: 4,
+                      weight: "regular"
+                    },
+                    {
+                      type: "text",
+                      text: logTimeHours + " Hr.",
+                      wrap: true,
+                      size: "lg",
+                      flex: 5,
+                      color: "#FF0000"
+                    }
+                  ]
+                },
+                {
+                  type: "box",
+                  layout: "baseline",
+                  spacing: "sm",
+                  contents: [
+                    {
+                      type: "text",
+                      text: "Update Time",
+                      size: "md",
+                      flex: 4
+                    },
+                    {
+                      type: "text",
+                      text: new Date(),
+                      wrap: true,
+                      size: "md",
+                      flex: 5
+                    }
+                  ],
+                  paddingTop: "xl"
+                }
+              ],
+              paddingTop: "xl"
+            }
+          ]
+        }
       }
     }]
   }
